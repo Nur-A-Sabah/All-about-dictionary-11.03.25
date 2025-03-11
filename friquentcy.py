cc2e6528c44e80166b7b65117c5ea44bad8a8956
Nur-A-Sabah <sajedurkabir@gmail.com>
2025-03-11 22:12:06 +0600

    first commit

---
+++ friquentcy.py
@@ -0,0 +1,17 @@
+test_dict = {
+    "Codingal" : 2,
+    "is" : 2,
+    "Best" : 5,
+    "for" : 2,
+    "Coding" : 1
+}
+print(f"The original dictionary : {test_dict}")
+
+k = 2
+res = 0
+
+for key in test_dict:
+    if test_dict[key] == k:
+        res = res + 1
+print(f"Freguency of K {k} is : {res}")
+    
